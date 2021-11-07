c34ec300465fb3850571d7a6903c105080a02cd5
ciaraweston <cweston0727@outlook.com>
2021-11-07 00:04:23 -0400

    first push of socialmediaclone

---
+++ config/database.js
@@ -0,0 +1,5 @@
+// config/database.js
+module.exports = {
+	url: 'mongodb+srv://cweston0727:RIpcpqZAFy0W9qQu@cluster0.ahgdx.mongodb.net/gardnerclone?retryWrites=true&w=majority',
+	dbName: 'gardnerGram'
+};
